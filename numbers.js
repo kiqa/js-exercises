0501224c22bd977d2996ad9b86c590b3237b4cad
kiqa <kellyalanya@hotmail.com>
2015-07-16 01:24:27 -0400

    numbers divisible by 3 and 5 in for loop

---
+++ numbers.js
@@ -0,0 +1,15 @@
+var number= [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+
+for (var i=0; i < number.length; i++) {
+    if (number[i] % 3 === 0 && number[i] % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (number[i] % 3 === 0) {
+        console.log("Fizz");
+    } else if (number[i] % 5 === 0) {
+        console.log("Buzz");
+    }
+    else {
+        console.log(number[i]);
+    }
+        
+}
